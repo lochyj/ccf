458bce44ffc998ef0520c360c6d5d088d4b64e66
Lachlan Jowett <lachlan@jowett.net.au>
2023-05-25 22:17:03 +1000

    working on frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Root from "./routes/root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./404";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
